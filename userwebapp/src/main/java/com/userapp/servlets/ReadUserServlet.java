c54ba244702e71245d447cb3eb616c8c94f2b02f
Bollineni <vbollineni@teksystems.com>
2022-04-18 13:05:29 +0530

    login

--- userwebapp/src/main/java/com/userapp/servlets/ReadUserServlet.java
+++ userwebapp/src/main/java/com/userapp/servlets/ReadUserServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ReadUserServlet")
+@WebServlet("/readServlet")
 public class ReadUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection connection;
