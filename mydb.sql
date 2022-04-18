3473fb485eec42e917df83d9f5670f5d94a01088
Bollineni <vbollineni@teksystems.com>
2022-04-18 11:23:08 +0530

    database

---
+++ mydb.sql
@@ -0,0 +1,5 @@
+create database mydb;
+use mydb;
+create table account(Accno int,Firstname varchar(25),Lastname varchar(25),Balance int);
+create table user(FirstName varchar(25),LastName varchar(25),Email varchar(25),Password varchar(25));
+select * from user;
